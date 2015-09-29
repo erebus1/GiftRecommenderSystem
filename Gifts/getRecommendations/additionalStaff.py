7a42f4435982a555d98043d395e7b237d59c2281
erebus1 <erebus@mail.ua>
2015-09-29 22:01:22 +0300

    save files to output folder

--- Gifts/getRecommendations/additionalStaff.py
+++ Gifts/getRecommendations/additionalStaff.py
@@ -2,12 +2,14 @@ import pickle
 
 __author__ = 'user'
 
-def save_to_file(var, filename = 'response.json'):
-    file = open(filename, 'w')
+
+def save_to_file(var, filename='response.json'):
+    file = open("output/" + filename, 'w')
     pickle.dump(var, file)
     file.close()
 
+
 def extract_from_file(filename="response.json"):
-    file = open(filename, 'r')
+    file = open("output/" + filename, 'r')
     var = pickle.load(file)
     return var
