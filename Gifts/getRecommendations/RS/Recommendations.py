191d1547e649b3576cd43a2e0171d981bd976ba2
erebus1 <erebus@mail.ua>
2015-09-30 17:42:41 +0300

    bson library updated

--- Gifts/getRecommendations/RS/Recommendations.py
+++ Gifts/getRecommendations/RS/Recommendations.py
@@ -1,4 +1,4 @@
-from bson import ObjectId
+from bson.objectid import ObjectId
 from Gifts.getRecommendations.Requests import findingApi
 from Gifts.getRecommendations.TextClasterisation import nlp
 from Gifts.getRecommendations.DB import DB
